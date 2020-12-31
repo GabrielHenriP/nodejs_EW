96cc761d83f5b750591c5f12596560ff3ecae732
gabriel <soadhenri@gmail.com>
2020-12-31 09:49:22 -0300

    webscraping and download pdf

--- 00-webScraping/ex2.js
+++ 00-webScraping/ex2.js
@@ -1,32 +1,32 @@
-/*const puppeteer = require('puppeteer')
-let scrape = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('http://books.toscrape.com/')
-  const result = await page.evaluate(() => {
-    const books = []
-    document.querySelectorAll('section > div > ol > li img')
-            .forEach((book) => books.push(book.getAttribute('alt')))
-    return books
-  })
-  browser.close()
-  return result
-}
-scrape().then((value) => {
-  console.log(value)
-})*/
+const download = require('download-pdf');
+const puppeteer = require('puppeteer');
 
-const puppeteer = require('puppeteer')
-let scrape = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar/padrao-tiss-dezembro-2020')
-  const result = await page.$eval('div.table-responsive table.table-bordered tbody tr td a', a => a.href )
-  browser.close()
-  return result
+let getLink = async () => {
+    
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  await page.goto('http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar/padrao-tiss-dezembro-2020');
+  const link = await page.$eval('a.btn.btn-primary.btn-sm.center-block', a => a.href );
+
+  browser.close();
+
+  return link;
 };
-scrape().then((value) => {
-  console.log(value)
-}).catch(error => {
+
+getLink()
+  .then(pdfLink => {
+    console.log(pdfLink)
+    let options = {
+      filename: "b.pdf"
+    }
+
+    download(pdfLink, options, err => {
+      if(err){
+        console.error(err)
+      }
+    }) 
+  })
+  .catch(error => {
     console.error(error)
-})
+  })
