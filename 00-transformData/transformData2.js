a1d77a01cac64f241f8fb1925d04714da9bd2c3d
gabriel <soadhenri@gmail.com>
2021-01-02 09:46:35 -0300

    generate csv files from objects

--- 00-transformData/transformData2.js
+++ 00-transformData/transformData2.js
@@ -1,31 +1,36 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const ObjectsToCsv = require('objects-to-csv');
  
 let dataBuffer = fs.readFileSync('Padrao_TISS_Componente_Organizacional__202012.pdf');
 
 pdf(dataBuffer).then( data => {
     const arrayPhrases = data.text.split('\n').filter( phrase => {
         return /[A-Za-z0-9]/.test(phrase)
-    })
+    }).map(phrase => phrase.trim())
 
-    let indexs = [];
+    let indexsToSlice = [];
     arrayPhrases.forEach( (phrase, index) => {
-        phrase.trim()
+        
         if(phrase.indexOf("Quadro 30") !== -1 || phrase.indexOf("5 ANS") !== -1)
-            indexs.push(index)  
+            indexsToSlice.push(index)  
     })
-    const arrayTable_1 = arrayPhrases.slice(indexs[0]+4, indexs[1]+1).map( phrase => {
-        return phrase.slice(2, phrase.length-1)
-    })
+    const arrayTable_1 = arrayPhrases.slice(indexsToSlice[0]+4, indexsToSlice[1]+1)
+        .map( phrase => {
+            let data = {
+                'Código': phrase.split(" ").slice(0,1).join(" "),
+                'Descrição da categoria': phrase.split(" ").slice(1,phrase.length).join(" ")
+            }
+            return data
+        })
     
 
-    indexs = [];
+    indexsToSlice = [];
     arrayPhrases.forEach( (phrase, index) => {
-        phrase.trim()
         if(phrase.indexOf("Quadro 31") !== -1 || phrase.indexOf("168 Guia de tratamento odontológico") !== -1)
-            indexs.push(index)  
+            indexsToSlice.push(index)  
     })
-    const arrayTable_2 = arrayPhrases.slice(indexs[0]+3, indexs[1]+1).map(phrase=>phrase.trim())
+    const arrayTable_2 = arrayPhrases.slice(indexsToSlice[0]+3, indexsToSlice[1]+1)
         .filter( phrase => {
             return phrase.length > 2 && phrase !== 'Padrão TISS - Componente Organizacional – dezembro de 2020'
         })
@@ -47,31 +52,34 @@ pdf(dataBuffer).then( data => {
             }
           
             return data
-          })  
+        })  
 
-    indexs = [];
+    indexsToSlice = [];
     arrayPhrases.forEach( (phrase, index) => {
         phrase.trim()
         if(phrase.indexOf("3 Exclusão") !== -1)
-            indexs.push(index)  
+            indexsToSlice.push(index)  
     })
-    const arrayTable_3 = arrayPhrases.slice(indexs[0]-2, indexs[0]+1).map( phrase => {
-        return phrase.slice(2, phrase.length-1)
-    })
-
-    const tables = [arrayTable_1, arrayTable_2, arrayTable_3]
-
-    const tablesObject = tables.map( table => {
-        return table.map( (phrase, index) => {
+    const arrayTable_3 = arrayPhrases.slice(indexsToSlice[0]-2, indexsToSlice[0]+1)
+        .map( phrase => {
             let data = {
-                codigo: index+1,
-                descricao: phrase
+                'Código': phrase.split(" ").slice(0,1).join(" "),
+                'Descrição da categoria': phrase.split(" ").slice(1,phrase.length).join(" ")
             }
             return data
         })
+
+    const tables = [arrayTable_1, arrayTable_2, arrayTable_3]
+    
+    let  numberTable = 30
+    const tablesCsv = tables.map( table => {
+        (async () => {
+
+            const csv = new ObjectsToCsv(table);
+            await csv.toDisk(`./Teste_Intuitive_Care_Gabriel/Quadro_${numberTable}.csv`);
+
+          })();
+          numberTable++;
     })
 
-    console.log(arrayTable_2)
-   
-    
 })
